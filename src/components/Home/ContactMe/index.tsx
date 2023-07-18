b6a72806f0681129c11c7c2ee341ba68eeae2f2a
William2103 <william2103@nexondv.com>
2023-07-18 14:23:36 +0700

    email

--- src/components/Home/ContactMe/index.tsx
+++ src/components/Home/ContactMe/index.tsx
@@ -7,23 +7,16 @@ import {
   SimpleGrid,
 } from '@mantine/core'
 import { IconDevicesPlus } from '@tabler/icons-react'
-import { useForm } from '@mantine/form'
 import EarthCanvas from 'src/components/canvas/Earth'
+import { TransformedValues, hasLength, isEmail, useForm } from '@mantine/form'
+import emailjs from '@emailjs/browser'
+import { useRef, useState } from 'react'
 import { useStyles } from './styles'
 
 export default function ContactMe() {
   const { classes, cx } = useStyles()
-import { Box, Title, Text, ActionIcon, Button, Group, TextInput, Textarea } from '@mantine/core';
-import { IconDevicesPlus, IconCertificate, IconHeadset } from '@tabler/icons-react';
-import { TransformedValues, hasLength, isEmail, useForm } from '@mantine/form';
-import emailjs from '@emailjs/browser';
-import { useRef, useState } from 'react';
-import { useStyles } from './styles';
-
-export default function ContactMe() {
-  const { classes, cx } = useStyles();
-  const [isLoadingBtn, setIsLoadingBtn] = useState(false);
-  const form = useRef<HTMLFormElement | null>(null);
+  const [isLoadingBtn, setIsLoadingBtn] = useState(false)
+  const form = useRef<HTMLFormElement | null>(null)
   const formContactMe = useForm({
     initialValues: {
       email: '',
@@ -35,12 +28,15 @@ export default function ContactMe() {
     validate: {
       name: hasLength({ min: 2, max: 30 }, 'Name must be 2-30 characters long'),
       email: isEmail('Invalid email'),
-      message: hasLength({ min: 15, max: 999 }, 'Message must be 15-999 characters long'),
+      message: hasLength(
+        { min: 15, max: 999 },
+        'Message must be 15-999 characters long'
+      ),
       phone: hasLength({ min: 9, max: 15 }, 'Phone must be between 9 and 15'),
     },
-  });
+  })
   const handleSubmit = (values: TransformedValues<typeof formContactMe>) => {
-    setIsLoadingBtn(true);
+    setIsLoadingBtn(true)
     emailjs
       .sendForm(
         'service_cgaaqem', // service id
@@ -49,17 +45,17 @@ export default function ContactMe() {
         'nQ-RxdlXS7bMR0nGf' // puclic key
       )
       .then(
-        (result) => {
-          console.log(result.text, values);
+        result => {
+          console.log(result.text, values)
         },
-        (error) => {
-          console.log(error.text);
+        error => {
+          console.log(error.text)
         }
       )
       .finally(() => {
-        setIsLoadingBtn(false);
-      });
-  };
+        setIsLoadingBtn(false)
+      })
+  }
   return (
     <Box className={classes.contactMeWrapper}>
       <Title data-heading="Get in touch" className="title">
@@ -84,26 +80,34 @@ export default function ContactMe() {
         >
           <TextInput
             name="user_name"
-            className={cx([{ 'input-not-empty': formContactMe.values.name.length > 0 }])}
+            className={cx([
+              { 'input-not-empty': formContactMe.values.name.length > 0 },
+            ])}
             label="Your name"
             {...formContactMe.getInputProps('name')}
           />
           <TextInput
             name="user_email"
-            className={cx([{ 'input-not-empty': formContactMe.values.email.length > 0 }])}
+            className={cx([
+              { 'input-not-empty': formContactMe.values.email.length > 0 },
+            ])}
             label="Email"
             {...formContactMe.getInputProps('email')}
           />
           <TextInput
             name="user_phone"
-            className={cx([{ 'input-not-empty': formContactMe.values.phone.length > 0 }])}
+            className={cx([
+              { 'input-not-empty': formContactMe.values.phone.length > 0 },
+            ])}
             label="Phone"
             type="number"
             {...formContactMe.getInputProps('phone')}
           />
           <Textarea
             name="user_message"
-            className={cx([{ 'input-not-empty': formContactMe.values.message.length > 0 }])}
+            className={cx([
+              { 'input-not-empty': formContactMe.values.message.length > 0 },
+            ])}
             label="Message"
             {...formContactMe.getInputProps('message')}
           />
